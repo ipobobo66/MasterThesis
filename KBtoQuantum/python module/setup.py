14c897b42fc60ebbf0d63b6d46aa7dc76df3812f
ipobobo66 <ipobobo66@gmail.com>
2025-10-04 12:41:47 +0200

    written setup.py, and tested the procedure in module_test_run.ipynb

---
+++ KBtoQuantum/python module/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="QsamplingFromLogical",        # Name of your package
+    version="0.1",
+    py_modules=["QsamplingFromLogical"],  # The file name (without .py)
+    author="Simon Botond",
+    description="Quantum logic sampling tools",
+    # Optional extras:
+    # install_requires=[],   # List required dependencies as Python list of strings
+    # url="https://github.com/yourname/yourrepo",
+    # classifiers=[],
+)
